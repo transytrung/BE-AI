25685936199df0b063b139c9ad16a5d55167461a
transytrung <148263907+transytrung@users.noreply.github.com>
2024-11-12 10:10:43 +0700

    Update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import University, Program, FAQ
 
-# Register your models here.
+
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'website')
+    search_fields = ('name', 'location')
+    list_filter = ('location',)  
+    ordering = ('name',)  
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('name', 'university', 'tuition_fee', 'duration', 'language_of_instruction')
+    list_filter = ('university', 'language_of_instruction', 'duration')
+    search_fields = ('name', 'university__name')
+    ordering = ('name', 'university')
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'program')
+    search_fields = ('question', 'answer', 'program__name')
+    list_filter = ('program',)
+    ordering = ('program', 'question')
